b7fe591698e2abe58008580f678c61bf555a56ea
Jack Serrino <Jserrino@gmail.com>
2015-01-08 22:26:47 -0500

    Add Application Complete boolean

--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -4,4 +4,8 @@ class Hacker < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def application_complete?
+    first_name and last_name and phone and school and shirt_size
+  end
+
 end
