4ff3f2c11cd06e4e6f83e4ab62da190976ca4be9
Jack Serrino <Jserrino@gmail.com>
2015-01-20 01:02:38 -0500

    Alter email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,10 +85,12 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
-    port: 465,
+    port: 587,
+    domain: "hackmit.org",
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    authentication: :login,
+    authentication: :plain,
+    enable_starttls_auto: true,
   }
   config.action_mailer.default_url_options = { host: 'apply.blueprint.hackmit.org' }
 end
